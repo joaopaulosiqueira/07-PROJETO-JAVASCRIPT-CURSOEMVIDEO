0563c6b5da11787ec71309f57daf503a5abc7228
João Paulo <77357142+joaopaulosiqueira@users.noreply.github.com>
2023-05-12 17:31:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[0]
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'Estou aguardando...'
